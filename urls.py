5275e188c39f6f6dd244165f38c6dd378ec68536
Taktix <taktix@gmail.com>
2010-04-04 10:29:43 -0700

    more css layout changes, added admin site

--- urls.py
+++ urls.py
@@ -3,14 +3,17 @@ import settings
 from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
     #authentication
     (r'^accounts/login/$', login),
     (r'^accounts/logout/$', logout, {'next_page':'/'}),
+
+    # admin site
+    (r'^admin/', include(admin.site.urls)),
 
     (r'^/*', include('raw.urls')),
     (r'^tv/*', include('tv.urls')),
