a3ddd7038e34339710beaff2123f174fc079aecd
Taktix <taktix@gmail.com>
2010-05-23 15:53:33 -0700

    fixing nzb download url mapping

--- raw/urls.py
+++ raw/urls.py
@@ -5,7 +5,7 @@ from views import *
 urlpatterns = patterns('',    
     (r'^$', posts),
     (r'^nfo/$', nfo),
-    (r'^nzb/(\d+)/$', nzb_download),
+    (r'^nzb/(.*)/$', nzb_download),
     (r'^related/(.*)/$', find_related),
     (r'^search/', search)
 )
